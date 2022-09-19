bf07089503210b45ff3d5b6cf6af2a2d573f1a83
MateuszKokot <ever1942@gmail.com>
2022-09-20 00:15:55 +0200

    Added API DataSheet and GroupController

--- app/Http/Controllers/API/GroupController.php
+++ app/Http/Controllers/API/GroupController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Group;
+use App\Models\KeyWord;
+use App\Models\Member;
+use App\Models\Tag;
+use App\Models\Word;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
@@ -14,7 +19,7 @@ class GroupController extends Controller
      */
     public function index()
     {
-        //
+        return "index";
     }
 
     /**
@@ -25,7 +30,42 @@ class GroupController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Tworzenie grupy
+        $newGroup = new Group;
+        $newGroup->firebase_chat_id = $request->firebase_chat_id;
+        $newGroup->name = $request->name;
+        $newGroup->description = $request->description;
+        $newGroup->coordinates = $request->coordinates;
+        $newGroup->date = $request->date;
+        $newGroup->max_members = $request->max_members;
+        $newGroup->save();
+
+        // Tworzenie memberów grupy
+        $members = $request->members;
+        foreach ($members as $member){
+            $newMembers = new Member();
+            $newMembers->group_id = $newGroup->id;
+            $newMembers->user_id = $member;
+            $membersForContent[] =
+            $newMembers->save();
+        }
+
+
+        // Tworzenie tagów grupy
+        $tags = $request->tags;
+        foreach ($tags as $tag){
+            $keyWord = KeyWord::where('keyword', $tag)->get();
+            $keywordsForContent[] = $keyWord[0]->keyword;
+            $newTags = new Tag();
+            $newTags->group_id = $newGroup->id;
+            $newTags->keyword_id = $keyWord[0]->id;
+            $newTags->save();
+        }
+
+        $content['group'] = Group::where('id', $newGroup->id)->get();
+        $content['keywords'] = $keywordsForContent;
+        $content['members'] = "members"; //TODO dorobić zwracanie membersów
+        return response($content);
     }
 
     /**
@@ -36,7 +76,7 @@ class GroupController extends Controller
      */
     public function show($id)
     {
-        //
+        return "show";
     }
 
     /**
@@ -48,7 +88,7 @@ class GroupController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        return "update";
     }
 
     /**
@@ -59,6 +99,6 @@ class GroupController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return "destory";
     }
 }
